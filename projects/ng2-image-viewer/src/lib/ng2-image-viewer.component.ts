33da449547dfc108b47a8d4065ceb4834dc7b353
Fisnik Limani <fisnik.limani@sorsix.com>
2021-10-07 16:07:55 +0200

    generate initial workspace and library

---
+++ projects/ng2-image-viewer/src/lib/ng2-image-viewer.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng2-image-viewer',
+  template: `
+    <p>
+      ng2-image-viewer works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class Ng2ImageViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/ng2-image-viewer/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng2-image-viewer
+ */
+
+export * from './lib/ng2-image-viewer.service';
+export * from './lib/ng2-image-viewer.component';
+export * from './lib/ng2-image-viewer.module';
