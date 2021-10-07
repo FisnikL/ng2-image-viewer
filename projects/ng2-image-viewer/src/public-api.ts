10dcfb007d94b08938c630164e0123931cdc8dd4
Fisnik Limani <fisnik.limani@sorsix.com>
2021-10-07 15:50:59 +0200

    clean again

--- projects/ng2-image-viewer/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng2-image-viewer
- */
-
-export * from './lib/ng2-image-viewer.service';
-export * from './lib/ng2-image-viewer.component';
-export * from './lib/ng2-image-viewer.module';
